984dd7af9d7a553ba448a58b0cac6d3797947994
Emirhan Kumus <emirhankumus@gmail.com>
2026-02-11 13:10:46 +0300

    Solved the first challenge of the workintech :)

--- demo.py
+++ demo.py
@@ -1,5 +1,10 @@
-
+"""
+Bu modül daire alanı hesaplamak için kullanılır.
+"""
+import math
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    """Returns the area of the circle of given radius. Returns 0 if radius is negative."""
+    if radius < 0:
+        return 0
+    return math.pi * (radius ** 2)
